a9dd47ba14112ac6f0364dcf9c413ebdb045ffdf
indrajit <eendroroy@gmail.com>
2017-06-08 08:18:07 +0600

    use plib git

--- modules/bgjob.zsh
+++ modules/bgjob.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 am_bg_count() {
-  echo -ne "%F{$am_fade_color}`plib_bg_count`%f "
+  echo -ne "%F{$am_fade_color}`plib_bg_count`%f"
 }
--- modules/git.zsh
+++ modules/git.zsh
@@ -1,49 +1,21 @@
 #!/usr/bin/env zsh
 
 am_is_git(){
-  if [[ $(\git branch 2>/dev/null) != "" ]]; then
-    echo 1
-  else
-    echo 0
-  fi
+  echo -ne `plib_is_git`
 }
 
 am_git_branch(){
-  ref=$(\git symbolic-ref HEAD 2> /dev/null) || ref="detached" || return;
-  echo -ne "%B%F{$am_branch_color}${ref#refs/heads/}%f%b";
+  echo -ne "%B%F{$am_branch_color}`plib_git_branch`%f%b";
 }
 
 am_git_rev(){
-  rev=$(\git rev-parse HEAD | cut -c 1-7);
-  echo -n "%F{$am_rev_color}${rev}%f";
+  echo -n "%F{$am_rev_color}`plib_git_rev`%f";
 }
 
 am_git_dirty(){
-  _mod=$(\git status --porcelain 2>/dev/null | grep 'M ' | wc -l | tr -d ' ');
-  _add=$(\git status --porcelain 2>/dev/null | grep 'A ' | wc -l | tr -d ' ');
-  _del=$(\git status --porcelain 2>/dev/null | grep 'D ' | wc -l | tr -d ' ');
-  _new=$(\git status --porcelain 2>/dev/null | grep '?? ' | wc -l | tr -d ' ');
-  [[ "$_mod" != "0" ]] && echo -n "%F{$am_dirty_color} ⭑%f";
-  [[ "$_add" != "0" ]] && echo -n "%F{$am_dirty_color} +%f";
-  [[ "$_del" != "0" ]] && echo -n "%F{$am_dirty_color} -%f";
-  [[ "$_new" != "0" ]] && echo -n "%F{$am_dirty_color} ?%f";
-
-  unset _mod _new _add _del
+  echo -n "%F{$am_dirty_color}`plib_git_dirty`%f";
 }
 
 am_git_left_right(){
-  function _branch(){
-    ref=$(\git symbolic-ref HEAD 2> /dev/null) || ref="detached" || return;
-    echo -ne "${ref#refs/heads/}";
-  }
-  if [[ $(am_git_branch) != "detached" ]]; then
-    _pull=$(\git rev-list --left-right --count `_branch`...origin/`_branch` 2>/dev/null | awk '{print $2}' | tr -d ' \n');
-    _push=$(\git rev-list --left-right --count `_branch`...origin/`_branch` 2>/dev/null | awk '{print $1}' | tr -d ' \n');
-    [[ "$_pull" != "0" ]] && [[ "$_pull" != "" ]] && echo -n "%F{$am_left_right_color} ▼%f";
-    [[ "$_push" != "0" ]] && [[ "$_push" != "" ]] && echo -n "%F{$am_left_right_color} ▲%f";
-
-    unset _pull _push _branch
-  else
-    echo -n "sdfsd";
-  fi
+  echo -n "%F{$am_left_right_color}`plib_git_left_right`%f";
 }
