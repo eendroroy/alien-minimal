65add43bfee5cca71f000e23701e7e6e1903f91b
indrajit <eendroroy@gmail.com>
2017-06-11 14:41:08 +0600

    update rpromot

--- modules/async.zsh
+++ modules/async.zsh
@@ -2,7 +2,7 @@
 
 am_set_r_prompt(){
   if [[ $(am_is_git) == 1 ]]; then
-      echo -ne "`am_bg_count`%F{$am_vcs_color}G%f `am_git_branch`(`am_git_commit_time`) `am_git_rev``am_git_left_right``am_git_dirty`"
+      echo -ne "`am_bg_count`%F{$am_vcs_color}G%f `am_git_branch``am_git_commit_time` `am_git_rev``am_git_left_right``am_git_dirty`"
   elif [[ $(am_is_hg) == 1 ]]; then
     echo -ne "`am_bg_count`%F{$am_vcs_color}M%f `am_hg_branch` `am_hg_rev`"
   elif [[ $(am_is_svn) == 1 ]]; then
