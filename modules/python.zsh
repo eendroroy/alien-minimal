fd2ce92e0ead3db4a0f753d634fe3212e4a6f996
indrajit <eendroroy@gmail.com>
2017-08-15 02:59:13 +0600

    python versions prompt

--- modules/python.zsh
+++ modules/python.zsh
@@ -3,3 +3,11 @@
 am_venv(){
   echo -ne "%F{$am_venv_color}`plib_venv`%f "
 }
+
+am_python_version(){
+  echo -ne "%F{$am_python_color}`plib_python_version`%f"
+}
+
+am_python_major_version(){
+  echo -ne "%F{$am_python_color}`plib_python_major_version`%f"
+}
