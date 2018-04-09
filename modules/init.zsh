c269cda62b021784501f3ce5d53e8a7685005adf
indrajit <eendroroy@gmail.com>
2018-04-10 00:44:26 +0600

    update symbols

--- modules/init.zsh
+++ modules/init.zsh
@@ -1,5 +1,23 @@
 #!/usr/bin/env zsh
 
+if [[ $USE_NERD_FONT = 1 ]]; then
+  [[ -z "${AM_GIT_SYM}" ]]        && AM_GIT_SYM=
+  [[ -z "${AM_HG_SYM}" ]]         && AM_HG_SYM=
+  [[ -z "${AM_SSH_SYM}" ]]        && AM_SSH_SYM=
+  [[ -z "${AM_JAVA_SYM}" ]]       && AM_JAVA_SYM=' '
+  [[ -z "${AM_PY_SYM}" ]]         && AM_PY_SYM=' '
+  [[ -z "${AM_RB_SYM}" ]]         && AM_RB_SYM=' '
+  [[ -z "${AM_NODE_SYM}" ]]       && AM_NODE_SYM=' '
+
+  [[ -z "${PLIB_GIT_ADD_SYM}" ]]  && PLIB_GIT_ADD_SYM=
+  [[ -z "${PLIB_GIT_DEL_SYM}" ]]  && PLIB_GIT_DEL_SYM=
+  [[ -z "${PLIB_GIT_MOD_SYM}" ]]  && PLIB_GIT_MOD_SYM=
+  [[ -z "${PLIB_GIT_NEW_SYM}" ]]  && PLIB_GIT_NEW_SYM=
+  [[ -z "${PLIB_GIT_PUSH_SYM}" ]] && PLIB_GIT_PUSH_SYM=
+  [[ -z "${PLIB_GIT_PULL_SYM}" ]] && PLIB_GIT_PULL_SYM=
+fi
+
+[[ -z "${AM_GIT_SYM}" ]]     && AM_GIT_SYM=G
 [[ -z "${AM_JAVA_SYM}" ]]    && AM_JAVA_SYM=''
 [[ -z "${AM_PY_SYM}" ]]      && AM_PY_SYM=''
 [[ -z "${AM_RB_SYM}" ]]      && AM_RB_SYM=''
@@ -7,25 +25,9 @@
 [[ -z "${AM_CRYSTAL_SYM}" ]] && AM_CRYSTAL_SYM=''
 [[ -z "${AM_NODE_SYM}" ]]    && AM_NODE_SYM='⬡ '
 [[ -z "${AM_PHP_SYM}" ]]     && AM_PHP_SYM=''
-[[ -z "${AM_GIT_SYM}" ]]     && AM_GIT_SYM=G
 [[ -z "${AM_HG_SYM}" ]]      && AM_HG_SYM=H
 [[ -z "${AM_SVN_SYM}" ]]     && AM_SVN_SYM=S
 [[ -z "${AM_SSH_SYM}" ]]     && AM_SSH_SYM=[S]
-
-if [[ $USE_NERD_FONT = 1 ]]; then
-  [[ -z "${AM_GIT_SYM}" ]]        && AM_GIT_SYM=
-  [[ -z "${PLIB_GIT_ADD_SYM}" ]]  && PLIB_GIT_ADD_SYM=
-  [[ -z "${PLIB_GIT_DEL_SYM}" ]]  && PLIB_GIT_DEL_SYM=
-  [[ -z "${PLIB_GIT_MOD_SYM}" ]]  && PLIB_GIT_MOD_SYM=
-  [[ -z "${PLIB_GIT_NEW_SYM}" ]]  && PLIB_GIT_NEW_SYM=
-  [[ -z "${PLIB_GIT_PUSH_SYM}" ]] && PLIB_GIT_PUSH_SYM=
-  [[ -z "${PLIB_GIT_PULL_SYM}" ]] && PLIB_GIT_PULL_SYM=
-  [[ -z "${AM_HG_SYM}" ]]         && AM_HG_SYM=
-  [[ -z "${AM_SSH_SYM}" ]]        && AM_SSH_SYM=
-  [[ -z "${AM_JAVA_SYM}" ]]       && AM_JAVA_SYM=' '
-  [[ -z "${AM_PY_SYM}" ]]         && AM_PY_SYM=' '
-  [[ -z "${AM_RB_SYM}" ]]         && AM_RB_SYM=' '
-fi
 
 [[ -z "${AM_GIT_REBASING_SYMBOL}" ]]  && AM_GIT_REBASING_SYMBOL='⇋ '
 
