1258ba2e20ac03b747817cd4292d9984af24b9b7
indrajit <eendroroy@gmail.com>
2025-04-22 11:09:57 +0600

    refactor

--- modules/sections_vcs.zsh
+++ modules/sections_vcs.zsh
@@ -227,13 +227,3 @@ am_svn_rev(){
 am_svn_symbol(){
   echo -ne "%F{$AM_VCS_COLOR}${AM_SVN_SYM}:%f"
 }
-
-am_ssh_st(){
-  __ssh_st=$(plib_ssh_st)
-  if [[ ${__ssh_st} != "" ]]
-	then
-    echo -n "%F{$AM_SSH_COLOR}${AM_SSH_SYM}%f"
-  else
-    echo -ne ""
-  fi
-}
